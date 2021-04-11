25fd7200892cb3488cf9f6754cbca3f2d70887c9
xugaoyi <894072666@qq.com>
2021-04-11 14:39:50 +0800

    feat: 添加登录接口中间件

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,19 @@
+/**
+ * 中间件文件 (修改此文件需要重新启动json-server服务)
+ */
+
+// 发送请求前的拦截
+module.exports = (req, res, next) => {
+  if (req.method === "POST" && req.path === "/login") {
+    if (req.body.username === "jack" && req.body.password === "123456") {
+      return res.status(200).json({
+        user: {
+          token: "123",
+        },
+      });
+    } else {
+      return res.status(400).json({ message: "用户名或者密码错误" });
+    }
+  }
+  next();
+};
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,7 +1,30 @@
 import { FormEvent } from "react";
+const apiUrl = process.env.REACT_APP_API_URL; // 读取环境变量参数
 
 export const LoginScreen = () => {
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {};
+  // 请求登录接口
+  const login = (param: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (response) => {
+      if (response.ok) {
+      }
+    });
+  };
+
+  // 处理登录
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // 阻止默认事件
+    const inputsEl = event.currentTarget.elements;
+    const username = (inputsEl[0] as HTMLInputElement).value;
+    const password = (inputsEl[1] as HTMLInputElement).value;
+    login({ username, password });
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -25,8 +48,8 @@ export const LoginScreen = () => {
 //   name: string
 // }
 
-// // 这里声明Base类型并没有报错，只要是传入参数类型是在接口以及继承就接口上就可以。 这称之为：鸭子类型： TS是面向接口编程 而不是 面向对象编程
 // const test = (p: Base) => { }
 
-// const a = { id: 1, name: '123' }
+// // 这里的并没有报类型错误，只要传入参数包含Base类型内的id就可以。 这称之为：鸭子类型： TS是面向接口编程 而不是 面向对象编程
+// const a = { id: 1, name: '123', c: '234' }
 // test(a)
